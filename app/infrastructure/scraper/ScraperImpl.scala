45c44882448bcaaef94372655186b34ff739e2b6
ikeikeikeike <jp.ne.co.jp@gmail.com>
2018-01-07 00:10:20 +0900

    bugfix

--- app/infrastructure/scraper/ScraperImpl.scala
+++ app/infrastructure/scraper/ScraperImpl.scala
@@ -4,12 +4,12 @@ import javax.inject.Inject
 
 import domain.scraper._
 import org.apache.commons.codec.binary.Base64
-import play.api.libs.json.{ JsError, JsSuccess }
-import play.api.libs.ws.{ WSClient, WSResponse }
-import play.api.{ Configuration, Logger }
+import play.api.libs.json.{JsError, JsSuccess}
+import play.api.libs.ws.{WSClient, WSResponse}
+import play.api.{Configuration, Logger}
 
-import scala.concurrent.duration.{ Duration, HOURS }
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.duration.{Duration, HOURS}
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Random
 
 class ScraperImpl @Inject() (config: Configuration, ws: WSClient) extends Scraper {
@@ -58,13 +58,14 @@ class ScraperImpl @Inject() (config: Configuration, ws: WSClient) extends Scrape
     tags = root.gatheredTags.mkString(",")
   )
 
-  private def toUrl(endpoint: String, src: String): String = {
-    val url = s"$endpoint${new String(Base64.encodeBase64(src.getBytes))}"
-    logger.info(s"${src} to be encoded $url")
+  private def toUrl(base: String, src: String): String = {
+    val url = s"$base${new String(Base64.encodeBase64(src.getBytes))}"
+    logger.info(s"$src to be encoded $url")
     url
   }
 
   private def lifecycle()(implicit ec: ExecutionContext): Future[Lifecycle] = {
+    logger.info(s"lifecycle: ${endpoint}")
     for (r <- ws.url(endpoint).get()) yield {
       val lifecycles = (r.json \ "root").as[Seq[Lifecycle]]
       Random.shuffle(lifecycles).head
